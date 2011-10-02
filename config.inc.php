42641af016fb37d1d383e1940a3bb733551abe50
Marius Orcsik <marius@habarnam.ro>
2011-10-02 19:13:02 +0300

    * keeping config just for local

--- config.inc.php
+++ config.inc.php
@@ -8,19 +8,11 @@ define ('LOCAL_RES_PATH', realpath('../res/') . DIRECTORY_SEPARATOR);
 
 include (LOCAL_RES_PATH . 'functions.inc.php');
 
-if (stristr($_SERVER['REMOTE_ADDR'], '127.0.0.1') == true) { // a hard-coded isDevelopment
-	define ('VSC_PATH', realpath ('/home/habarnam/workspace/vsc-v2/') . DIRECTORY_SEPARATOR);
-	define ('DB_HOST', 'localhost');
-	define ('DB_USER', 'root');
-	define ('DB_PASS', 'asd');
-	define ('DB_NAME', 'htlm');
-} else {
-	define ('VSC_PATH', realpath ('/srv/http/vsc-v2-no-sql/') . DIRECTORY_SEPARATOR);
-	define ('DB_HOST', 'localhost');
-	define ('DB_USER', 'htlm');
-	define ('DB_PASS', ';\'asd');
-	define ('DB_NAME', 'htlm');
-}
+define ('VSC_PATH', realpath ('/home/habarnam/workspace/vsc-v2/') . DIRECTORY_SEPARATOR);
+define ('DB_HOST', 'localhost');
+define ('DB_USER', 'root');
+define ('DB_PASS', 'asd');
+define ('DB_NAME', 'htlm');
 
 date_default_timezone_set('Europe/Bucharest');
 // set function overloading for unicode
