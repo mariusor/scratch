3053acc4dd2a10034c1a07bf05c5518d38a32d3d
Marius Orcsik <marius@habarnam.ro>
2011-11-19 18:56:59 +0200

    * making the littr code dependent on the new vsc orm structure

--- config.inc.php
+++ config.inc.php
@@ -9,6 +9,7 @@ define ('LOCAL_RES_PATH', realpath('../res/') . DIRECTORY_SEPARATOR);
 include (LOCAL_RES_PATH . 'functions.inc.php');
 
 define ('VSC_PATH', realpath ('/home/habarnam/workspace/vsc-v2/') . DIRECTORY_SEPARATOR);
+define ('ORM_PATH', realpath ('/home/habarnam/workspace/orm/') . DIRECTORY_SEPARATOR);
 define ('DB_HOST', 'localhost');
 define ('DB_USER', 'root');
 define ('DB_PASS', 'asd');
@@ -23,7 +24,10 @@ if (!@include(VSC_PATH . 'vsc.inc.php')) {
 	throw new ErrorException('Could not load libVSC');
 }
 
+
 import (LOCAL_LIB_PATH);
 import (LOCAL_RES_PATH);
+
+import (ORM_PATH);
 
 set_error_handler('exceptions_error_handler');
