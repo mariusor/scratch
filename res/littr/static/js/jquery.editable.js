a62da6d2e0378fbefc99f8d1fa0877d5393e7fec
Marius Orcsik <marius@habarnam.ro>
2011-11-27 12:04:40 +0200

    * improved horizontal rule

--- res/littr/static/js/jquery.editable.js
+++ res/littr/static/js/jquery.editable.js
@@ -797,7 +797,7 @@ Sanitize.prototype.clean_node = function (container) {
 									callback(prompt("Enter image URL:", "http://"));
 							}
 					},
-					inserthorizontalrule: {shortcut: "Ctrl+Alt+h", execCommand: "inserthtml", execCommandValue: ['<hr/>']},
+					inserthorizontalrule: {shortcut: "Ctrl+Alt+h", execCommand: "inserthorizontalrule"},
 					strikethrough: {shortcut: "Ctrl+Alt+t", execCommand: "strikethrough"},
 					increaseFontSize: {shortcut: "Ctrl+Alt+=", execCommand: "increasefontsize"},
 					decreaseFontSize: {shortcut: "Ctrl+Alt+m", execCommand: "decreasefontsize"}, // keyCode for - seems to be interpreted as M
