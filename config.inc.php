c8e74ff3755158a78a167fc08ac43d2f9ca146c7
Marius Orcsik <marius@habarnam.ro>
2013-09-24 23:09:55 +0300

    Config changes for local vsc orm

--- config.inc.php
+++ config.inc.php
@@ -9,7 +9,7 @@ define ('LOCAL_RES_PATH', realpath('../res/') . DIRECTORY_SEPARATOR);
 include (LOCAL_RES_PATH . 'functions.inc.php');
 
 define ('VSC_PATH', realpath ('/home/habarnam/workspace/vsc-v2/') . DIRECTORY_SEPARATOR);
-define ('ORM_PATH', realpath ('/home/habarnam/workspace/orm/') . DIRECTORY_SEPARATOR);
+define ('ORM_PATH', realpath ('/home/habarnam/workspace/vsc-orm/') . DIRECTORY_SEPARATOR);
 
 date_default_timezone_set('Europe/Bucharest');
 // set function overloading for unicode
@@ -17,7 +17,12 @@ date_default_timezone_set('Europe/Bucharest');
 
 // including lib vsc
 if (!@include(VSC_PATH . 'vsc.inc.php')) {
-	throw new ErrorException('Could not load libVSC');
+	throw new ErrorException('Could not load libVSC at path [' . VSC_PATH . ']');
+}
+
+// including lib vsc
+if (!@include(ORM_PATH. 'orm.inc.php')) {
+	throw new ErrorException('Could not load libVSC ORM at path [' . ORM_PATH . ']');
 }
 
 import (LOCAL_LIB_PATH);
