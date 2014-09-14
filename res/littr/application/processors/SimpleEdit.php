618f1086da8a87b75629871c1f6db58581c3ca11
Marius Orcsik <marius@habarnam.ro>
2014-09-14 15:34:07 +0300

    Adding a dynamic title for pages containing <h1/> headers

--- res/littr/application/processors/SimpleEdit.php
+++ res/littr/application/processors/SimpleEdit.php
@@ -2,8 +2,8 @@
 namespace littrme\littr\application\processors;
 
 use littrme\littr\domain\models\EditModel;
+use Masterminds\HTML5;
 use vsc\infrastructure\urls\UrlRWParser;
-use vsc\infrastructure\vsc;
 use vsc\presentation\requests\HttpRequestA;
 use littrme\application\processors\Processor;
 
@@ -28,6 +28,18 @@ class SimpleEdit extends Processor {
 
 		$sCurrentUri = urldecode($oUri->getPath());
 		$oModel = new EditModel($sCurrentUri);
+
+		if (extension_loaded('dom')) {
+			$html5 = new HTML5();
+			$DOMDoc = $html5->loadHTML ( $oModel->content );
+
+			$Titles = $DOMDoc->getElementsByTagName ( 'h1' );
+			if ( $Titles->length > 0 ) {
+				$sTitle = trim ( $Titles->item ( 0 )->textContent );
+
+				$this->getMap ()->setTitle ( $sTitle );
+			}
+		}
 
 		return $oModel;
 	}
