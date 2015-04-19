a0bc3c9ca470cab6637317f59a515f44457e8b7c
Marius Orcsik <marius@habarnam.ro>
2015-04-19 15:47:29 +0200

    Removed String->StringUtils in redirect to rand

--- res/littr/application/processors/RedirectToRand.php
+++ res/littr/application/processors/RedirectToRand.php
@@ -2,8 +2,8 @@
 namespace littrme\littr\application\processors;
 
 use vsc\application\processors\ProcessorA;
+use vsc\infrastructure\StringUtils;
 use vsc\infrastructure\urls\UrlRWParser;
-use vsc\infrastructure\vsc;
 use vsc\presentation\requests\HttpRequestA;
 use vsc\presentation\responses\ExceptionResponseRedirect;
 use vsc\infrastructure\String;
@@ -16,7 +16,7 @@ class RedirectToRand extends ProcessorA {
 
 	public function handleRequest (HttpRequestA $oHttpRequest) {
 		$oUrl = UrlRWParser::getCurrentUrl();
-		$oUrl->addPath(String::baseEncode(intval(microtime(true) * 10000)));
+		$oUrl->addPath(StringUtils::baseEncode(intval(microtime(true) * 10000)));
 		$oUrl->setQueryParameters(null);
 
 		throw new ExceptionResponseRedirect($oUrl->getCompleteUri(true));
