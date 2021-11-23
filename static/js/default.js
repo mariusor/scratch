97dd4e7a8257ffd0063bb8462e9fbb5505141e34
Marius Orcsik <marius@federated.id>
2021-11-23 14:57:53 +0100

    Improved JS for check/save key
    Disabled functionality we don't care about at the moment

--- static/js/default.js
+++ static/js/default.js
@@ -1,20 +1,55 @@
 const warning = 'Since there is no content, this page will be deleted once you close the tab or browser window.';
 $(document).ready( function() {
-	const w = $(window);
 	const uri = $(location).attr('href');
 
 	let editable = $("body > section:first-child");
-	editable.attr("contentEditable", true);
+	editable.unlocked = function() {
+		return editable.prop('contentEditable') == "true"
+	};
+	editable.lock = function () {
+		editable.attr("contentEditable", false);
+	};
+	editable.unlock = function () {
+		editable.attr("contentEditable", true);
+	};
+	editable.emptyContent = function () {
+		const that = $(this);
+		if (editable.unlocked()) {
+			let lastModified = that.data('modified');
+
+			if (typeof (lastModified) == 'undefined') {
+				lastModified = that.attr('data-modified');
+			}
+			const d = new Date(lastModified);
+			if (d.toString() == 'Invalid Date') {
+				previousContent = ' ';
+				that.html(previousContent);
+			}
+		}
+	};
 
 	const lock = $('<svg aria-hidden="true" class="icon"><use xlink:href="/icons.svg#icon-lock"><title>Locked</title></use></svg>')
 	const unlock = $('<svg aria-hidden="true" class="icon"><use xlink:href="/icons.svg#icon-unlock"><title>Unlocked</title></use></svg>')
 
-	let icon = unlock;
-	const a = $('<a/>').addClass('hidden').append(icon);
-	const feedBack = $("<nav/>").addClass('feedback').append(a);
-	$("body").prepend(feedBack);
+	const a = $('<a/>').addClass('hidden');
+	a.click (function (e) {
+		const message = 'Please enter the secret key for this page.';
+		authToken = prompt (message, '');
+		if (authToken != null) {
+			let icon = a.find("svg");
+			let isLocked = icon.find("title").text() == "Locked";
+			console.debug("is Locked: %s %s", icon.find("title").text(), isLocked);
+			if (isLocked) {
+				console.debug("checking auth token: %s", authToken);
+				checkForSecrets();
+			} else {
+				console.debug("saving auth token: %s", authToken);
+				saveKey();
+			}
+		}
+	});
 
-	// lock-unlock icon
+	const feedBack = $("<nav/>").addClass('feedback').append(a);
 	feedBack.mouseenter(function(e) {
 		a.fadeIn(1200, () => { a.addClass("hidden"); });
 	}).mouseleave(function (e) {
@@ -24,22 +59,9 @@ $(document).ready( function() {
 			a.fadeIn(1200).fadeOut(1200);
 		}
 	});
+	$("body").prepend(feedBack);
 
-	editable.emptyContent = function () {
-		const that = $(this);
-		if (that.attr('contentEditable') == 'true') {
-			let lastModified = $(this).data('modified');
-
-			if (typeof (lastModified) == 'undefined') {
-				lastModified = $(this).attr('data-modified');
-			}
-			const d = new Date(lastModified);
-			if (d.toString() == 'Invalid Date') {
-				previousContent = ' ';
-				that.html(previousContent);
-			}
-		}
-	};
+	editable.unlock();
 
 	const titleText = editable.attr('title');
 
@@ -51,29 +73,15 @@ $(document).ready( function() {
 	let bStillSaving = false;
 	let selection = null;
 
-	a.click (function (e) {
-		const message = 'Please enter the secret key for this page.';
-		authToken = prompt (message, '');
-		if (authToken != null) {
-			let isLocked = icon.find("title").text() == "Locked";
-			if (!isLocked) {
-				console.debug("saving auth token: %s", authToken);
-				saveKey();
-			} else {
-				console.debug("checking auth token: %s", authToken);
-				checkForSecrets();
-			}
-		}
-	});
 
 	function removeOnClose(e) {
-		$.ajax({
+		let request = $.ajax({
 			url: uri,
 			type: 'delete',
 			beforeSend: setAuthorizationToken,
-			complete: function (jqXHR, status) {
-				console.debug("deleted %s: %s", uri, status)
-			}
+		});
+		request.always((jqXHR, status) => {
+			console.debug("deleted %s: %s", uri, status)
 		});
 		return warning;
 	}
@@ -110,7 +118,7 @@ $(document).ready( function() {
 	// adding click events to make links to work in edit mode
 	editable.find('a').mousedown(function(e) {
 		const la = $(e.target);
-		if (la.is('a') && editable.attr ('contentEditable') == 'true') {
+		if (la.is('a') && editable.unlocked()) {
 			e.preventDefault();
 			e.stopPropagation();
 			switch (e.detail) {
@@ -125,7 +133,156 @@ $(document).ready( function() {
 
 	checkForSecrets();
 
-	const id = setInterval(() => { save(); }, waitTime);
+	setInterval(() => { save(); }, waitTime);
+
+	function setAuthorizationToken(xhr) {
+		if (authToken != null) {
+			xhr.setRequestHeader("Authorization", authToken);
+		}
+	};
+
+	function checkForSecrets() {
+		console.debug("check secrets: %s", authToken)
+		let request = $.ajax({
+			url: uri,
+			type: 'head',
+			beforeSend: setAuthorizationToken,
+		});
+		request.done(() => {
+			a.empty();
+			a.append(unlock);
+			editable.attr("contentEditable", true);
+			console.debug("unlocked");
+		});
+		request.fail((xhr) => {
+			if (xhr.status == 401) {
+				a.empty();
+				a.append(lock);
+				editable.attr("contentEditable", false);
+				console.debug("locked");
+			}
+		});
+	};
+
+	function unsavedChanges (text) {
+		return text != previousContent;
+	};
+
+	function resetTimer (xhr, status) {
+		bStillSaving = false;
+		finish = new Date();
+		const lastRun = finish.getTime() - start.getTime();
+		let multiplier = 2;
+		if (lastRun > 5000) {
+			multiplier = 0.1;
+		} else if (lastRun > 1000) {
+			multiplier = 1;
+		} else if (lastRun < 400) {
+			multiplier = 10;
+		} else if (lastRun < 100) {
+			multiplier = 20;
+		}
+		waitTime = lastRun * multiplier;
+	};
+
+	function saveKey () {
+		let request = $.ajax({
+			url: uri,
+			type: 'patch',
+			beforeSend: function (xhr) {
+				bStillSaving = true;
+				setAuthorizationToken(xhr);
+			},
+		});
+
+		request.done(() => {
+			console.debug(request);
+		});
+		request.fail((xhr) => {
+			console.error("failed to update key:", xhr);
+		});
+		request.always(resetTimer);
+	};
+
+	function save () {
+		console.debug ("next check: in %dms", waitTime);
+		console.debug ("still saving: %s", (bStillSaving ? 'yes' : 'no'));
+		if (bStillSaving) {
+			return;
+		}
+		console.debug ("is editable: %s", editable.unlocked());
+		if (!editable.unlocked()) {
+			return;
+		}
+		const changes = unsavedChanges (editable.html());
+		console.debug ("changes: %s", changes);
+		if (!changes) {
+			return;
+		}
+		const now = new Date();
+		const overTheWaitTime = ((now.getTime() - finish.getTime()) > waitTime);
+		console.debug ("over the wait time: %s", overTheWaitTime);
+		if (!overTheWaitTime) {
+			return;
+		}
+		const modifiedLast = new Date(editable.data("modified"));
+		console.debug ("last modified: %sms ago", (now.getTime() - modifiedLast));
+		console.debug ("last save: %dms ago", (now.getTime() - finish.getTime()));
+
+		editable.fresheditor('save', function (id, content) {
+			const postData = {'content': content};
+			let request = $.ajax({
+				url: uri,
+				type: 'post',
+				data: postData,
+				beforeSend: function (xhr) {
+					start = new Date();
+					bStillSaving = true;
+					previousContent = content;
+					setAuthorizationToken(xhr);
+				},
+			});
+
+			request.done(() => {
+				console.debug(request);
+				if (request.status == 200) {
+					editable.data('modified', now.valueOf());
+				}
+			});
+			request.fail((xhr) => {
+				console.error("failed to update: %d", xhr.status, xhr);
+			});
+			request.always(resetTimer);
+		});
+	}
+
+	/*
+	function isSaveKey (e) {
+		//var moveKeys	= [33,34,35, 36, 37,38,39,40]; // pg-down, pg-up, end, home, left, up, right, down
+		const singleKeys	= [8,9,13,32,46,190]; // bksp, cr, space, tab, del, "." ,
+		const ctrlKeys	= [27, 83, 90]; // ctrl-v, ctrl-s, ctrl-z
+		const shiftKeys	= [16]; // shift-insert
+
+		if (e.ctrlKey && ctrlKeys.indexOf (e.keyCode) != -1) {
+			return true
+		}
+
+		if (e.shiftKey && shiftKeys.indexOf (e.keyCode) != -1) {
+			return true;
+		}
+
+		if (singleKeys.indexOf (e.keyCode) != -1) {
+			return true;
+		}
+		return false;
+	}
+
+	function handleDragOver(e) {
+		e.stopPropagation();
+		e.preventDefault();
+		const evt = e.originalEvent;
+		evt.dataTransfer.dropEffect = 'copy'; // Explicitly show this is a copy.
+	};
 
 	function handleFileSelect(e) {
 		e.stopPropagation();
@@ -169,158 +326,6 @@ $(document).ready( function() {
 		}
 	};
 
-	function handleDragOver(e) {
-		e.stopPropagation();
-		e.preventDefault();
-		const evt = e.originalEvent;
-		evt.dataTransfer.dropEffect = 'copy'; // Explicitly show this is a copy.
-	};
-
-	function setAuthorizationToken(xhr) {
-		if (authToken != null) {
-			xhr.setRequestHeader("Authorization", authToken);
-		}
-	};
-
-	function checkForSecrets() {
-		console.debug("check secrets: %s", authToken)
-		let request = $.ajax({
-			url: uri,
-			type: 'head',
-			beforeSend: setAuthorizationToken,
-		});
-		request.done(() => {
-			icon.replaceWith(unlock);
-			editable.attr("contentEditable", true);
-			console.debug("unlocked");
-		});
-		request.fail((xhr) => {
-			if (xhr.status == 401) {
-				icon.replaceWith(lock);
-				editable.attr("contentEditable", false);
-				console.debug("locked");
-			}
-		});
-	};
-
-	function unsavedChanges (text) {
-		return text != previousContent;
-	}
-
-	function saveKey () {
-		let now = new Date();
-		let request = $.ajax({
-			url: uri,
-			type: 'post',
-			beforeSend: function (xhr) {
-				bStillSaving = true;
-				setAuthorizationToken(xhr);
-			},
-		});
-
-		request.done(() => {
-			console.debug(request);
-		});
-		request.fail((xhr) => {
-			console.error("failed to update: %d %s", xhr.status, xhr);
-		});
-		request.always((xhr, status) => {
-			bStillSaving = false;
-			finish = new Date();
-			const lastRun = finish.getTime() - start.getTime();
-			var multiplier = 2;
-			if (lastRun > 5000) {
-				multiplier = 0.1;
-			} else if (lastRun > 1000) {
-				multiplier = 1;
-			} else if (lastRun < 400) {
-				multiplier = 10;
-			} else if (lastRun < 100) {
-				multiplier = 20;
-			}
-			waitTime = lastRun * multiplier;
-		});
-	};
-
-	function save () {
-		let now = new Date();
-		console.debug ("saving: %s", (bStillSaving ? 'yes' : 'no'));
-		console.debug ("is editable %s", (editable.prop('contentEditable') == "true" ? 'yes' : 'no'));
-		console.debug ("changes: %s", (unsavedChanges (editable.html()) ? 'yes' : 'no'));
-		console.debug ("last save: %dms ago", (now.getTime() - finish.getTime()));
-		console.debug ("last modified: %s", new Date(editable.data("modified")));
-		console.debug ("will save?: %s", (editable.prop("contentEditable") == "true" && !bStillSaving && unsavedChanges(editable.html()) && ((now.getTime() - finish.getTime()) > waitTime) ? 'yes' : 'no'));
-		console.debug ("next check: %dms", waitTime);
-		if (
-			editable.prop("contentEditable") == "true" &&
-			!bStillSaving &&
-			unsavedChanges(editable.html()) &&
-			((now.getTime() - finish.getTime()) > waitTime)
-		) {
-			editable.data('modified', now.getTime());
-			editable.fresheditor('save', function (id, content) {
-				const postData = {'content': content};
-
-				let request = $.ajax({
-					url: uri,
-					type: 'post',
-					data: postData,
-					beforeSend: function (xhr) {
-						start = new Date();
-						bStillSaving = true;
-						previousContent = content;
-						setAuthorizationToken(xhr);
-					},
-				});
-
-				request.done(() => {
-					console.debug(request);
-					if (request.status == 200) {
-						editable.data('modified', now.valueOf());
-					}
-				});
-				request.fail((xhr) => {
-					console.error("failed to update: %d", xhr.status, xhr);
-				});
-				request.always(() => {
-					bStillSaving = false;
-					finish = new Date();
-					const lastRun = finish.getTime() - start.getTime();
-					var multiplier = 2;
-					if (lastRun > 5000) {
-						multiplier = 0.1;
-					} else if (lastRun > 1000) {
-						multiplier = 1;
-					} else if (lastRun < 400) {
-						multiplier = 10;
-					} else if (lastRun < 100) {
-						multiplier = 20;
-					}
-					waitTime = lastRun * multiplier;
-				});
-			});
-		}
-	}
-
-	function isSaveKey (e) {
-		//var moveKeys	= [33,34,35, 36, 37,38,39,40]; // pg-down, pg-up, end, home, left, up, right, down
-		const singleKeys	= [8,9,13,32,46,190]; // bksp, cr, space, tab, del, "." ,
-		const ctrlKeys	= [27, 83, 90]; // ctrl-v, ctrl-s, ctrl-z
-		const shiftKeys	= [16]; // shift-insert
-
-		if (e.ctrlKey && ctrlKeys.indexOf (e.keyCode) != -1) {
-			return true
-		}
-
-		if (e.shiftKey && shiftKeys.indexOf (e.keyCode) != -1) {
-			return true;
-		}
-
-		if (singleKeys.indexOf (e.keyCode) != -1) {
-			return true;
-		}
-		return false;
-	}
-
 	function pad(n) {return n<10 ? '0'+n : n}
+	*/
 });
