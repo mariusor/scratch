334075323302d890690f1e2f178bf546fd4f9447
Marius Orcsik <marius@federated.id>
2024-10-12 14:53:01 +0200

    Removed unused Write functions from the internal assets package

--- internal/assets/assets_dev.go
+++ internal/assets/assets_dev.go
@@ -3,16 +3,10 @@
 package assets
 
 import (
-	"errors"
-	"fmt"
+	"git.sr.ht/~mariusor/assets"
 	"io/fs"
-	"mime"
-	"net/http"
 	"os"
 	"path/filepath"
-	"time"
-
-	"git.sr.ht/~mariusor/assets"
 )
 
 var (
@@ -23,29 +17,3 @@ var (
 
 	TemplateFS, _ = fs.Sub(assetFS, "templates")
 )
-
-func Write(s fs.FS, errFn func(http.ResponseWriter, *http.Request, ...error)) func(http.ResponseWriter, *http.Request) {
-	const cacheTime = 8766 * time.Hour
-
-	mime.AddExtensionType(".ico", "image/vnd.microsoft.icon")
-	mime.AddExtensionType(".txt", "text/plain; charset=utf-8")
-	return func(w http.ResponseWriter, r *http.Request) {
-		asset := r.RequestURI
-		mimeType := mime.TypeByExtension(filepath.Ext(asset))
-
-		buf, err := fs.ReadFile(s, asset)
-		if err != nil {
-			if errors.Is(err, os.ErrNotExist) {
-				err = fmt.Errorf("not found: %s %w", asset, err)
-			}
-			errFn(w, r, err)
-			return
-		}
-
-		w.Header().Set("Cache-Control", fmt.Sprintf("public,max-age=%d", int(cacheTime.Seconds())))
-		if mimeType != "" {
-			w.Header().Set("Content-Type", mimeType)
-		}
-		w.Write(buf)
-	}
-}
--- internal/assets/assets_prod.go
+++ internal/assets/assets_prod.go
@@ -2,47 +2,6 @@
 
 package assets
 
-import (
-	"errors"
-	"fmt"
-	"io/fs"
-	"mime"
-	"net/http"
-	"os"
-	"path/filepath"
-	"time"
-)
+import "io/fs"
 
 var TemplateFS, _ = fs.Sub(FS, "templates")
-
-func Write(s fs.FS, errFn func(http.ResponseWriter, *http.Request, ...error)) func(http.ResponseWriter, *http.Request) {
-	const cacheTime = 8766 * time.Hour
-
-	assetContents := make(map[string][]byte)
-	mime.AddExtensionType(".ico", "image/vnd.microsoft.icon")
-	mime.AddExtensionType(".txt", "text/plain; charset=utf-8")
-	return func(w http.ResponseWriter, r *http.Request) {
-		asset := r.RequestURI
-		mimeType := mime.TypeByExtension(filepath.Ext(asset))
-
-		buf, ok := assetContents[asset]
-		if !ok {
-			cont, err := fs.ReadFile(s, asset)
-			if err != nil {
-				if errors.Is(err, os.ErrNotExist) {
-					err = fmt.Errorf("not found %s %w", asset, err)
-				}
-				errFn(w, r, err)
-				return
-			}
-			buf = cont
-		}
-		assetContents[asset] = buf
-
-		w.Header().Set("Cache-Control", fmt.Sprintf("public,max-age=%d", int(cacheTime.Seconds())))
-		if mimeType != "" {
-			w.Header().Set("Content-Type", mimeType)
-		}
-		w.Write(buf)
-	}
-}
